127243ab24fcf3d12b72d88a0f07c68a4eb6ddae
fuchodeveloper <fmgbeoma@yahoo.com>
2018-09-11 11:09:10 +0100

    try-catch-finally

--- src/try-catch/index.js
+++ src/try-catch/index.js
@@ -1,5 +1,5 @@
 // --- mutiple try..catch blocks
-
+/*
 const readData = () => {
   let json = '{"name": "Tesla"}';
   try {
@@ -16,3 +16,31 @@ try {
 } catch (error) {
   console.log('External catch: ' + error)
 }
+*/
+
+// -- try..catch..finally
+
+let num = -1;
+let diff, result, err;
+
+const fib = (n) => {
+  if (n < 0 || Math.trunc(n) !== n) {
+    throw new Error('Must not be negative, also must be an integer');
+  }
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+
+let start = Date.now();
+
+try {
+  result = fib(num);
+} catch (error) {
+  err = error;
+  result = 0;
+} finally {
+  diff = Date.now() - start;
+}
+
+console.log(result || 'an error occured: ' + err);
+
+console.log(`execution took ${diff}ms`);
