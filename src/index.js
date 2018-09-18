1844c2b9cc4c0f98fec0597ca7f9772c9b1372b7
fuchodeveloper <fmgbeoma@yahoo.com>
2018-09-18 12:53:34 +0100

    handling textarea and select tag

--- src/index.js
+++ src/index.js
@@ -4,39 +4,60 @@ import './index.css';
 // import App from './r-conditional-rendering/App';
 import App from './App';
 
-class NameForm extends Component {
+
+class EassyForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''}
+    this.state = {
+      value: 'Essay about fav DOM element',
+      selectValue: 'coconut'
+    };
   }
 
-
   handleChange = event => {
-    this.setState({value: event.target.value});
+    this.setState({[event.target.name]: event.target.value});
   }
 
   handleSubmit = event => {
-    console.log(`Value: ${this.state.value}`)
+    console.log(`Submission: ${this.state.value} | Fav: ${this.state.selectValue}`);
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Name: <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
+      <label>
+        Favorite color:
+        <select
+          multiple={true}
+          name="selectValue"
+          value={[this.state.selectValue]}
+          onChange={this.handleChange}
+        >
+          <option value="grapefruit">Grape fruit</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+      </label>
+      <br /> <br />
+      <label>
+        Essay: 
+        <textarea
+          name="value"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </label>
+      <br />
+      <input type="submit" value="Submit" />
+      <br />
       </form>
     )
   }
-
 }
+
 ReactDOM.render(
-  <NameForm/>,
+  <EassyForm />,
   document.getElementById('root')
 );
