ae71f293cfd2131a29feb172397be16d60058084
fuchodeveloper <fmgbeoma@yahoo.com>
2018-09-24 20:20:45 +0100

    react: lifting state example

--- src/index.js
+++ src/index.js
@@ -4,6 +4,63 @@ import './index.css';
 // import App from './r-conditional-rendering/App';
 import App from './App';
 
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit',
+};
+
+class TemperatureInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { temperature: '' };
+  }
+
+  handleChange = e => {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const { scale, temperature} = this.props;
+
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange}
+        />
+        {/* <BoilingVerdict
+          celsius={parseFloat(temperature)}
+        /> */}
+      </fieldset>
+    )
+  }
+}
+
+const toCelsius = fahrenheit => (fahrenheit - 32) * 5 / 9;
+
+const toFahrenheit = celsius => (celsius * 9 / 5) + 32;
+
+const tryConvert = (temperature, convert) => {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+class Calculator extends Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale='c' />
+        <TemperatureInput scale='f' />
+      </div>
+    )
+  }
+}
+
 const BoilingVerdict = (props) => {
   if (props.celsius >= 100) {
     return <p>Water would boil</p>
@@ -12,6 +69,6 @@ const BoilingVerdict = (props) => {
 }
 
 ReactDOM.render(
-  <BoilingVerdict />,
+  <Calculator />,
   document.getElementById('root')
 );
