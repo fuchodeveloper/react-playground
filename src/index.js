f4d181e2d0e3681867e2f9efcbd49ec98a88ebee
fuchodeveloper <fmgbeoma@yahoo.com>
2018-09-27 02:57:57 +0100

    React composition

--- src/index.js
+++ src/index.js
@@ -4,93 +4,33 @@ import './index.css';
 // import App from './r-conditional-rendering/App';
 import App from './App';
 
-const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit',
-};
-
-class TemperatureInput extends Component {
-  handleChange = e => {
-    this.props.onTemperatureChange(e.target.value);
-  }
-
-  render() {
-    const {scale, temperature} = this.props;
-
-    return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input
-          value={temperature}
-          onChange={this.handleChange}
-        />
-      </fieldset>
-    )
-  }
+function Contacts() {
+  return <div className="Contacts" />;
 }
 
-const toCelsius = fahrenheit => (fahrenheit - 32) * 5 / 9;
-
-const toFahrenheit = celsius => (celsius * 9 / 5) + 32;
-
-const tryConvert = (temperature, convert) => {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return '';
-  }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
-}
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {temperature: '', scale: ''}
-  }
-
-  handleCelsiusChange = temperature => {
-    this.setState({scale: 'c', temperature});
-  }
-
-  handleFahrenheitChange = temperature => {
-    this.setState({scale: 'f', temperature});
-  }
-  render() {
-    const {scale, temperature} = this.state;
-
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-
-    return (
-      <div>
-        <TemperatureInput 
-          scale='c'
-          temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange}
-        />
-
-        <TemperatureInput 
-          scale='f'
-          temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange}
-        />
-
-        <BoilingVerdict
-          celsius={parseFloat(celsius)}
-        />
-      </div>
-    )
-  }
+function Chat() {
+  return <div className="Chat" />;
 }
 
-const BoilingVerdict = (props) => {
-  if (props.celsius >= 100) {
-    return <p>Water would boil</p>
-  }
-  return <p>Water wouldn't boil.</p>
-}
+const SplitPane = props => (
+  <div className="SplitPane">
+    <div className="SplitPane-left">
+      {props.left}
+    </div>
+    <div className="SplitPane-right">
+      {props.right}
+    </div>
+  </div>
+);
+
+const App1 = () => (
+  <SplitPane
+    left={<Contacts />}
+    right={<Chat />}
+  />
+);
 
 ReactDOM.render(
-  <Calculator />,
+  <App1 />,
   document.getElementById('root')
 );
